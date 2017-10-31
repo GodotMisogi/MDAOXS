379f6d4d50640e068eba78cb8dd1c115e4ff5c6f
shixinsb <shixinsb@sina.com>
2017-10-31 08:20:00 +0100

    start

---
+++ basicOp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding=utf-8
+# *******************************************************************
+#     Filename @  basicOp.py
+#       Author @  xshi
+#  Change date @  10/31/2017 8:08 AM
+#        Email @  xshi@kth.se
+#  Description @  master thesis project
+# ********************************************************************
+"""
+Introduction
+"""
+import numpy as np
+#import matplotlib.pyplot as plt
+import scipy as sp
+import openmdao as om
+## cell
+cell = 1
+print(cell)
+##
+#%%
