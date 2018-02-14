aaa3d02624e97efdd59879eb910ceccbe0e0fe8d
xshiii <xshi@kth.se>
2018-02-14 20:12:04 +0100

    stl txt file to numpy

--- tools/txtIO.py
+++ tools/txtIO.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import plotly
 
 def stlTxtToNpArray(path):
     path = r"/Users/gakki/Downloads/SU2_mesh_point_clouds/Optimale_orig_points.txt"
