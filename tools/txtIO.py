ba14ff60d7d288dba5f189b7608c52934279dde0
xshiii <xshi@kth.se>
2018-02-14 20:10:04 +0100

    stl txt file to numpy

---
+++ tools/txtIO.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import plotly
+
+def stlTxtToNpArray(path):
+    path = r"/Users/gakki/Downloads/SU2_mesh_point_clouds/Optimale_orig_points.txt"
+    mesh = np.empty([1,3])
+    with open(path) as fp:
+        for line in fp:
+            if line.__len__()<5:
+                continue
+            line = line.strip().split(';')
+            line = list(map(str.strip, line))
+            if line[1].startswith('-'):
+                if line[1].count('-') == 2:
+                    line[1] = line[1].replace('-','e-')[1:]
+            else:
+                line[1] = line[1].replace('-','e-')
+            mesh = np.vstack([mesh,np.array(line,dtype='float')])
+    mesh = mesh[1:,:]
+    return mesh
+
+
+
+if __name__=='__main__':
+    path = r"/Users/gakki/Downloads/SU2_mesh_point_clouds/Optimale_orig_points.txt"
+    mesh = stlTxtToNpArray(path=path)
+    np.savetxt('new_mesh.txt',mesh,delimiter=';')
